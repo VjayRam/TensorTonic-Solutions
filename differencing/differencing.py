de8b89b8a99448e56d529e19054e262f763a9d2b
Vijay Ram <vjayram.enag@gmail.com>
2026-03-18 14:39:22 -0700

    Add solution for differencing

---
+++ differencing/differencing.py
@@ -0,0 +1,10 @@
+def differencing(series, order):
+    """
+    Apply d-th order differencing to the time series.
+    """
+    for _ in range(order):
+        series = [series[i] - series[i-1] for i in range(1, len(series))]
+
+    return series
+        
+    
